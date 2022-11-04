43432b95a1c1b268269240f3a9751088c24e8de5
ManganiBiagio <biagiomangani@live.it>
2022-11-04 17:52:53 +0100

    function infitiyy carosel

--- js/carousel.js
+++ js/carousel.js
@@ -1,28 +1,28 @@
-const images=[
+const images = [
     "img/01.webp",
     "img/02.webp",
     "img/03.webp",
     "img/04.webp",
     "img/05.webp"
 ]
-const boxImagesEl=document.querySelector(".img-box");
-const btnUpEl=document.querySelector(".btn-up");
-const btnDownEl=document.querySelector(".btn-down");
-let indice=0;
+const boxImagesEl = document.querySelector(".img-box");
+const btnUpEl = document.querySelector(".btn-up");
+const btnDownEl = document.querySelector(".btn-down");
+let indice = 0;
 
 //************************************************************** */
 //INIZIALIZZO IL CONTENUTO
-for(let i=0;i<images.length;i++){
-    let propriety="d-none";
-    if(i===indice){
-        propriety="d-block"
+for (let i = 0; i < images.length; i++) {
+    let propriety = "d-none";
+    if (i === indice) {
+        propriety = "d-block"
     }
-    const imgEl = document.createElement( "img" );
-    imgEl.classList.add( "my-img",propriety );
-    imgEl.src=images[i];
-    imgEl.alt="";   
+    const imgEl = document.createElement("img");
+    imgEl.classList.add("my-img", propriety);
+    imgEl.src = images[i];
+    imgEl.alt = "";
     boxImagesEl.append(imgEl);
-    
+
 
 }
 
@@ -32,41 +32,51 @@ for(let i=0;i<images.length;i++){
 
 //************************************************************** */
 //EVENTI
-btnUpEl.addEventListener("click",function(){
-    
-    const oldImage=document.querySelector(`.img-box :nth-child(${})`);
-    
-    const newImage=document.querySelector(`.img-box :nth-child(${})`);
+btnUpEl.addEventListener("click", function () {
+
     indice++;
-    
-    
+    let oldImage;
+    let newImage;
+    if (indice === images.length) {
+        let indice1=1;
+        let indice2=images.length;
+         oldImage = document.querySelector(`.img-box :nth-child(${indice2})`);
+         newImage = document.querySelector(`.img-box :nth-child(${indice1})`);
+         indice=0;
+    }
+    else {
+        oldImage= document.querySelector(`.img-box :nth-child(${indice})`);
+        newImage = document.querySelector(`.img-box :nth-child(${indice + 1})`);
+    }
+
+
     oldImage.classList.remove("d-block");
     oldImage.classList.add("d-none");
     newImage.classList.remove("d-none");
     newImage.classList.add("d-block");
-    
 
-    
-    
+
+
+
 
 
 })
 
-btnDownEl.addEventListener("click",function(){
-    const oldImage=document.querySelector(`.img-box :nth-child(${(indice-1)%images.length+1})`);
-    let newImage=document.querySelector(`.img-box :nth-child(${(indice-2)%images.length+1})`);
+btnDownEl.addEventListener("click", function () {
+    const oldImage = document.querySelector(`.img-box :nth-child(${(indice - 1) % images.length + 1})`);
+    let newImage = document.querySelector(`.img-box :nth-child(${(indice - 2) % images.length + 1})`);
     indice--;
     oldImage.classList.remove("d-block");
     oldImage.classList.add("d-none");
     newImage.classList.remove("d-none");
     newImage.classList.add("d-block");
-    
-
-    
-    
 
 
-    
+
+
+
+
+
 })
 
 //************************************************************** */

190b494a468bee5be80da674a8c81cfc7753ba44
ManganiBiagio <biagiomangani@live.it>
2022-11-06 12:28:06 +0100

    bonus completed

--- js/carousel.js
+++ js/carousel.js
@@ -6,9 +6,12 @@ const images = [
     "img/05.webp"
 ]
 const imagesEl = [];
+const anteprime = [];
 const boxImagesEl = document.querySelector(".img-box");
 const btnUpEl = document.querySelector(".btn-up");
 const btnDownEl = document.querySelector(".btn-down");
+const anteprimaImg = document.querySelector(".anteprima-box");
+
 let indice = 0;
 
 //************************************************************** */
@@ -24,7 +27,20 @@ for (let i = 0; i < images.length; i++) {
     imgEl.alt = "";
     boxImagesEl.append(imgEl);
     imagesEl[i] = imgEl;
+    const anteprimaEl = document.createElement("img");
+    anteprimaEl.src = images[i];
+    if (i === indice) {
+        anteprimaEl.classList.add("img-fluid", "anteprima-img", "img-active");
+    }
+    else {
+        anteprimaEl.classList.add("img-fluid", "anteprima-img");
+    }
 
+    anteprimaEl.style.height = `calc( 100% / ${images.length} )`;
+
+    anteprimaEl.alt = "";
+    anteprime[i] = anteprimaEl;
+    anteprimaImg.append(anteprimaEl);
 
 
 }
@@ -36,22 +52,29 @@ for (let i = 0; i < images.length; i++) {
 
 //************************************************************** */
 //EVENTI
-btnUpEl.addEventListener("click", function () {
+btnDownEl.addEventListener("click", function () {
 
-    
+    let oldAnteprima;
+    let newAnteprima;
     let oldImage;
     let newImage;
-    if (indice === images.length-1) {
+    if (indice === images.length - 1) {
         newImage = imagesEl[0];
-        oldImage = imagesEl[images.length-1];
+        oldImage = imagesEl[images.length - 1];
+        oldAnteprima = anteprime[images.length - 1];
+        newAnteprima = anteprime[0];
         indice = -1;
     }
     else {
         oldImage = imagesEl[indice];
         newImage = imagesEl[indice + 1];
+        oldAnteprima = anteprime[indice];
+        newAnteprima = anteprime[indice + 1];
 
     }
     indice++;
+    oldAnteprima.classList.remove("img-active");
+    newAnteprima.classList.add("img-active");
     oldImage.classList.remove("d-block");
     oldImage.classList.add("d-none");
     newImage.classList.remove("d-none");
@@ -59,21 +82,30 @@ btnUpEl.addEventListener("click", function () {
 
 })
 
-btnDownEl.addEventListener("click", function () {
-   
+btnUpEl.addEventListener("click", function () {
+
+    let oldAnteprima;
+    let newAnteprima;
     let oldImage;
     let newImage;
     if (indice === 0) {
-        newImage = imagesEl[images.length-1];
+        newImage = imagesEl[images.length - 1];
         oldImage = imagesEl[0];
+        oldAnteprima = anteprime[0];
+        newAnteprima = anteprime[images.length - 1];
         indice = images.length;
     }
     else {
         oldImage = imagesEl[indice];
         newImage = imagesEl[indice - 1];
+        oldAnteprima = anteprime[indice];
+        newAnteprima = anteprime[indice - 1];
+        
 
     }
     indice--;
+    oldAnteprima.classList.remove("img-active");
+    newAnteprima.classList.add("img-active");
     oldImage.classList.remove("d-block");
     oldImage.classList.add("d-none");
     newImage.classList.remove("d-none");

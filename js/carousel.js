c6f7d5f24a55ce00ce6118610e7f44f5d746b8e8
ManganiBiagio <biagiomangani@live.it>
2022-11-06 11:19:27 +0100

    carosele function

--- js/carousel.js
+++ js/carousel.js
@@ -5,6 +5,7 @@ const images = [
     "img/04.webp",
     "img/05.webp"
 ]
+const imagesEl = [];
 const boxImagesEl = document.querySelector(".img-box");
 const btnUpEl = document.querySelector(".btn-up");
 const btnDownEl = document.querySelector(".btn-down");
@@ -22,9 +23,12 @@ for (let i = 0; i < images.length; i++) {
     imgEl.src = images[i];
     imgEl.alt = "";
     boxImagesEl.append(imgEl);
+    imagesEl[i] = imgEl;
+
 
 
 }
+
 
 
 //************************************************************** */
@@ -34,52 +38,42 @@ for (let i = 0; i < images.length; i++) {
 //EVENTI
 btnUpEl.addEventListener("click", function () {
 
-    indice++;
+    
     let oldImage;
     let newImage;
-    if (indice === images.length) {
-        let indice1=1;
-        let indice2=images.length;
-         oldImage = document.querySelector(`.img-box :nth-child(${indice2})`);
-         newImage = document.querySelector(`.img-box :nth-child(${indice1})`);
-         indice=0;
+    if (indice === images.length-1) {
+        newImage = imagesEl[0];
+        oldImage = imagesEl[images.length-1];
+        indice = -1;
     }
     else {
-        oldImage= document.querySelector(`.img-box :nth-child(${indice})`);
-        newImage = document.querySelector(`.img-box :nth-child(${indice +1})`);
+        oldImage = imagesEl[indice];
+        newImage = imagesEl[indice + 1];
+
     }
-
-
+    indice++;
     oldImage.classList.remove("d-block");
     oldImage.classList.add("d-none");
     newImage.classList.remove("d-none");
     newImage.classList.add("d-block");
 
-
-
-
-
-
 })
 
 btnDownEl.addEventListener("click", function () {
-    indice--;
+   
     let oldImage;
     let newImage;
-    if (indice === -1 || indice ===0) {
-        let indice1=1;
-        let indice2=images.length;
-         oldImage = document.querySelector(`.img-box :nth-child(${indice1})`);
-         newImage = document.querySelector(`.img-box :nth-child(${indice2})`);
-         indice=images.length;
+    if (indice === 0) {
+        newImage = imagesEl[images.length-1];
+        oldImage = imagesEl[0];
+        indice = images.length;
     }
     else {
-        oldImage= document.querySelector(`.img-box :nth-child(${indice +1})`);
-        newImage = document.querySelector(`.img-box :nth-child(${indice  })`);
+        oldImage = imagesEl[indice];
+        newImage = imagesEl[indice - 1];
+
     }
-    
-
-
+    indice--;
     oldImage.classList.remove("d-block");
     oldImage.classList.add("d-none");
     newImage.classList.remove("d-none");
